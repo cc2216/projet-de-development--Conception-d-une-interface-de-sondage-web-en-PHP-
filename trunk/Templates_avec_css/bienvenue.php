7f0c5563c18825d71d2665b020d5052e269798c6
cchen <cchen@4d656549-a6f7-46b6-898f-73a53bf2c294>
2015-04-07 20:33:02 +0000

    Templates avec un peu de css

---
+++ trunk/Templates_avec_css/bienvenue.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="utf-8" />
+	<title>Bienvenue</title>
+    <link  rel="stylesheet" type="text/css" href="..\cssfile\css\style.css"/>
+    
+    
+	</head>
+    <style>
+
+    .center
+    {
+        margin:auto;
+        text-align: center;
+        width:70%;
+    }
+    header{
+        margin:auto;
+        text-align: left; 
+        font-size: 10pt;
+        color: black;
+        text-decoration: underline;
+    }
+    a:link {color:#FF0000;}    
+    a:visited {color:#00FF00;} 
+    html,body{margin:0;padding:0}
+    body{
+        font: 76% arial,sans-serif;
+        text-align:center;
+        position: relative;
+        }
+    .position{
+        left: 50%;
+        right:50%;
+    }
+    p{margin:0 10px 10px; text-align:center}
+    
+
+</style>
+	<body>	
+
+	<header>
+     <img src="../image/logo_telecom_bretagne.jpg" alt="Ballade" width="80" height="80"/>
+	Sondage Neuro-pshycologique
+	</header>
+
+	<section>
+	
+	<article>
+	<p style="text-align: center; font-size: 20pt; margin: 20px">
+	Bienvenue cher internaute.</p>
+	<p>Vous êtes sur le point de participer à un sondage neuro-psychologique, dont l'objectif est de mieux comprendre le fonctionnement de notre cerveau.<br/>
+	Ce petit quizz contient 20 questions sous formes de pistes audios, lesquelles vous devez identifier. Vous ne pourrez écouter une piste audio qu'une seule fois.<br/>
+	Merci pour votre temps.<br/>
+    </p>
+    </article>
+	
+	<aside>
+	<h1>Auteurs</h1>
+	Ce site a été réalisé, dans le cadre d'un projet à Telecom Bretagne, par: Affane Reda, Nam, Julien , et CHAO.....
+	</aside>
+	</section>
+	
+	<section class="center">
+	<h1>Veuillez renseigner ce formulaire:</h1>
+	<form method="post" action="formulaire.php">
+	<p>
+	
+	<label class="center" for="nom">Nom:</label>
+	<input type="text" name="nom" id="nom"/> 
+	<br/>
+	
+	<label class="center" for="prenom">Prenom:</label>
+	<input type="text" name="prenom" id="prenom"/> 
+	<br/>
+	
+	
+    <label class="center" for="masculin">Masculin:<input type="radio" name="sexe"  value="masculin" id="masculin"/> </label>
+	<label  class="center" for="feminin">Feminin:<input type="radio" name="sexe"  value="feminin" id="feminin"/></label>
+    
+	
+	<label class="center" for="mail">Adresse mail:</label>
+	<input type="email" name="mail" id="mail"/> 
+	<br/>
+	
+	<label class="center" for="date">Date de naissance:</label>
+	<input type="date" name="date_naissance" id="date"/> 
+	<br/>
+	
+	<label class="center" for="pays">Pays d'origine:</label>
+	<select name="pays" id="pays"> 
+		<option value="france">France</option>
+	    <option value="france">Maroc</option>
+		<option value="france">Chine</option>
+		<option value="france">Tunisie</option>
+	</select>	
+	<br/>
+	
+	<input type="submit" value="envoyer"/>
+	</p>
+	</form>
+	</section>
+ 	
+	
+	
+	
+	
+	
+	
+	<footer>
+	Contactez nous <a href="#">ici</a>
+	</footer>
+	</body>
+</html>
---
+++ trunk/Templates_avec_css/identification.php
@@ -0,0 +1,46 @@
+<?php
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=projetdev;charset=utf8', 'root', '');
+}
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}
+
+session_start;
+?>
+
+<!DOCTYPE html>
+<html>
+
+	<head>
+	<meta charset="utf-8" />
+	<title>Test identification</title>
+    <link  rel="stylesheet" type="text/css" href="..\cssfile\css\style.css"/>
+    <link  rel="stylesheet" type="text/css" href="test1.css"/>
+	</head>
+	
+	
+	<body>	
+	<header>
+     <img src="../image/logo_telecom_bretagne.jpg" alt="Ballade" width="80" height="80"/>
+	Test d'identification
+	</header>
+
+	
+	<body>
+	<p>Vous ecouterez le son seulement une fois. Veuillez identifier le son en ecrivant son nom.<p>
+	<audio src="../Sons/pluie.mp3" controls>Veuillez mettre ид jour votre navigateur !</audio>
+	<form method="post" action="identification_post.php">
+	<p>
+
+	<label class='center' for="reponse">Reponse</label>
+	<input type="text" name="reponse" id="reponse"/>
+	<br/>
+	<input type="submit" value="OK" /><br/>
+	
+	</p>
+	</body>
+</html>
+	
