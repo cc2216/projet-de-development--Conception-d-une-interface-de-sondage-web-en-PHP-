36eb9c5edb5f5736355006dc20ce9f4485792e25
cchen <cc2216@users.noreply.github.com>
2015-06-19 11:32:32 +0000

    ecouter une seule fois

--- gestion_V3/templates/tests/representativite.php
+++ gestion_V3/templates/tests/representativite.php
@@ -9,8 +9,21 @@
 	<p style="font-size:150%">Vous ecouterez 5 versions différentes d'un même son. Classez de 1 à 5, les sens qui vous sont le plus reprresentatifs.<p>
      
     <form method="post" action="templates/tests/representativite_post.php">
+    <audio id="audioPlayer1" width="300" height="32" src="./__SON1__"></audio>
+    <button onclick="play('audioPlayer1', this); this.onclick=' '"><img src="./templates/image/ecouter.png"></button>
+    <script type="text/javascript">
 
-	<audio  src="./__SON1__" controls>Veuillez mettre a jour votre navigateur !</audio>
+     function play(idAudioPlayer, context) {
+     var audioplayer = document.querySelector('#' + idAudioPlayer);
+
+    	if (audioplayer.played) {
+       		 audioplayer.play();
+        	 context.textContent = 'déjà écoutè';
+       		}
+       }
+
+       
+</script>
 	<label  style="display: inline" class='center' for="champ1"><strong>N°</strong></label>
 	<select name="champ1" id="champ1"> 
 	    <option value="none">Derouler</option>
@@ -23,7 +36,8 @@
 	<br/><br/>
 
 
-		<audio  src="./__SON2__" controls>Veuillez mettre a jour votre navigateur !</audio>
+	<audio id="audioPlayer2" width="300" height="32" src="./__SON2__"></audio>
+    <button onclick="play('audioPlayer2', this); this.onclick=''"><img src="./templates/image/ecouter.png"></button>
 	<label  style="display: inline" class='center' for="champ2"><strong>N°</strong></label>
 	<select name="champ2" id="champ2"> 
 	    <option value="none">Derouler</option>
@@ -36,7 +50,8 @@
 	<br/><br/>
 
 
-		<audio  src="./__SON3__" controls>Veuillez mettre a jour votre navigateur !</audio>
+	<audio id="audioPlayer3" width="300" height="32" src="./__SON3__"></audio>
+    <button onclick="play('audioPlayer3', this); this.onclick=''"><img src="./templates/image/ecouter.png"></button>
 	<label style="display: inline" class='center' for="champ3"><strong>N°</strong></label>
 	<select name="champ3" id="champ3"> 
 	    <option value="none">Derouler</option>
@@ -49,7 +64,8 @@
 	<br/><br/>
 
 
-		<audio  src="./__SON4__" controls>Veuillez mettre a jour votre navigateur !</audio>
+	<audio id="audioPlayer4" width="300" height="32" src="./__SON4__"></audio>
+    <button onclick="play('audioPlayer4', this); this.onclick=''"><img src="./templates/image/ecouter.png"></button>
 	<label style="display: inline" class='center' for="champ4"><strong>N°</strong></label>
 	<select name="champ4" id="champ4"> 
 	    <option value="none">Derouler</option>
@@ -62,7 +78,8 @@
 	<br/><br/>
 
 
-		<audio  src="./__SON5__" controls>Veuillez mettre a jour votre navigateur !</audio>
+	<audio id="audioPlayer5" width="300" height="32" src="./__SON5__"></audio>
+    <button onclick="play('audioPlayer5', this); this.onclick=''"><img src="./templates/image/ecouter.png"></button>
 	<label style="display: inline" class='center' for="champ5"><strong>N°</strong></label>
 	<select name="champ5" id="champ5"> 
 	    <option value="none">Derouler</option>
